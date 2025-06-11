f0ea2651b3aac3ec69a30b10d1c803767f1ae3a1
Harshika Parab <140838169+harshikaparab@users.noreply.github.com>
2025-06-12 00:59:47 +0530

    Update socket.js

--- client/src/services/socket.js
+++ client/src/services/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const socket = io(process.env.REACT_APP_SOCKET_URL);
+const socket = io('https://sports-fiesta-api.vercel.app/');
 
 export const subscribeToScores = (callback) => {
   socket.on('scoreUpdate', callback);
